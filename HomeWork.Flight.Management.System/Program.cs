b7605c4cda5dac2ea1d2949d3351c562948834d6
Kazım Göksel KALYONCU <goksel.kalyoncu@gmail.com>
2022-01-16 20:51:10 +0300

    Flight.Management.System

---
+++ HomeWork.Flight.Management.System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork.Flight.Management.System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
